435b90288af2c50de2835fbbdd39bca0fbf9515e
Yauhescha <evgescha.maslov007@yandex.ru>
2023-06-15 20:31:37 +0300

    MVP

---
+++ core/src/com/hescha/game/Flappee.java
@@ -0,0 +1,53 @@
+package com.hescha.game;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+
+public class Flappee {
+    private static final float COLLISION_RADIUS = 24f;
+    private static final float DIVE_ACCEL = 0.3f;
+    private static final float FLE_ACCEL = 5f;
+    private float ySpeed = 0;
+
+    private final Circle collisonCircle;
+
+    private float x = 0;
+    private float y = 0;
+
+    public Flappee() {
+        collisonCircle = new Circle(x, y, COLLISION_RADIUS);
+    }
+
+    public void drawDebug(ShapeRenderer shapeRenderer) {
+        shapeRenderer.circle(collisonCircle.x, collisonCircle.y, collisonCircle.radius);
+    }
+
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+        updateCollisionCircle();
+    }
+
+    private void updateCollisionCircle() {
+        collisonCircle.setX(x);
+        collisonCircle.setY(y);
+    }
+
+    public void update() {
+        ySpeed -= DIVE_ACCEL;
+        setPosition(x, y + ySpeed);
+    }
+
+    public void flyUp(){
+        ySpeed=FLE_ACCEL;
+        setPosition(x, y + ySpeed);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+}
--- core/src/com/hescha/game/GameScreen.java
+++ core/src/com/hescha/game/GameScreen.java
@@ -1,11 +1,15 @@
 package com.hescha.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -14,10 +18,15 @@ public class GameScreen extends ScreenAdapter {
     public static final float WORLD_WIDTH = 480;
     public static final float WORLD_HEIGHT = 640;
 
+    private static final float GAP_BETWEEN_FLOWERS = 200F;
+
     private ShapeRenderer shapeRenderer;
     private Viewport viewport;
     private Camera camera;
     private SpriteBatch batch;
+
+    private Flappee flappee = new Flappee();
+    private Array<Flower> flowers = new Array<>();
 
     @Override
     public void resize(int width, int height) {
@@ -32,19 +41,81 @@ public class GameScreen extends ScreenAdapter {
         viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
         shapeRenderer = new ShapeRenderer();
         batch = new SpriteBatch();
+        flappee.setPosition(WORLD_WIDTH / 4, WORLD_HEIGHT / 2);
     }
 
     @Override
     public void render(float delta) {
         clearScreen();
+        update(delta);
+
         batch.setProjectionMatrix(camera.projection);
         batch.setTransformMatrix(camera.view);
         batch.begin();
-
         batch.end();
+
+        shapeRenderer.setProjectionMatrix(camera.projection);
+        shapeRenderer.setTransformMatrix(camera.view);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        drawDebug();
+        shapeRenderer.end();
+    }
+
+    private void drawDebug() {
+        flappee.drawDebug(shapeRenderer);
+        for (Flower flower : flowers) {
+            flower.drawDebug(shapeRenderer);
+        }
+    }
+
+    private void update(float delta) {
+        flappee.update();
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            flappee.flyUp();
+        }
+        blockFlappeeLeavingTheWorld();
+        updateFlowers(delta);
+    }
+
+    private void updateFlowers(float delta) {
+        for (Flower flower : flowers) {
+            flower.update(delta);
+        }
+        checkIfNewFlowerIsNeeded();
+        removeFlowersIfPassed();
+    }
+
+    private void blockFlappeeLeavingTheWorld() {
+        flappee.setPosition(flappee.getX(), MathUtils.clamp(flappee.getY(), 0, WORLD_HEIGHT));
     }
 
     private void clearScreen() {
         ScreenUtils.clear(Color.BLACK);
+    }
+
+    private void createNewFlower() {
+        Flower newFlower = new Flower();
+        newFlower.setPosition(WORLD_WIDTH + Flower.WIDTH);
+        flowers.add(newFlower);
+    }
+
+    private void checkIfNewFlowerIsNeeded() {
+        if (flowers.size == 0) {
+            createNewFlower();
+        } else {
+            Flower flower = flowers.peek();
+            if (flower.getX() < WORLD_WIDTH - GAP_BETWEEN_FLOWERS) {
+                createNewFlower();
+            }
+        }
+    }
+
+    private void removeFlowersIfPassed(){
+        if(flowers.size>0){
+            Flower first = flowers.first();
+            if(first.getX()<-Flower.WIDTH){
+                flowers.removeValue(first, true);
+            }
+        }
     }
 }
