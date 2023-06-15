86f449a8ab01097020f7b85725d1b2dc750579e8
Yauhescha <evgescha.maslov007@yandex.ru>
2023-06-15 19:03:01 +0300

    Initial commit

---
+++ core/src/com/hescha/game/FlappeeBee.java
@@ -0,0 +1,12 @@
+package com.hescha.game;
+
+
+import com.badlogic.gdx.Game;
+
+public class FlappeeBee extends Game {
+
+    @Override
+    public void create() {
+        setScreen(new GameScreen());
+    }
+}
